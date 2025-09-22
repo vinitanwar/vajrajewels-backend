1014d627e01290f519d135146e74af982ac2df6f
vivek <157991083+jonty1231@users.noreply.github.com>
2025-09-23 02:04:13 +0530

    done

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://vajrajewels.com','http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\SiteuserController;
 use App\Http\Controllers\TestimonialController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+ 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
